2309f64cdd968d5708ec3f68f63aac7677d5ae57
Angelo <angvil02@gmail.com>
2024-10-14 16:08:07 -0700

    Fixed head.js test cases to test for empty arrays rather than undefined

--- head.js
+++ head.js
@@ -15,9 +15,9 @@ const head = function(arr) {
 
 
 // TEST CODE
-assertEqual(head([4, 5, 3, 2, 7]), 4)
+assertEqual(head([4, 5, 3, 2, 7]), 4);
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1)
-assertEqual(head(), 3)
-assertEqual(head(), "test")
+assertEqual(head([1]), 1);
+assertEqual(head([]), 3);
+assertEqual(head([]), "test");
