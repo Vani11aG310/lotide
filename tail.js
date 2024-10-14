d62bb57569f73e19b021a0def870fe2d30d52456
Angelo <angvil02@gmail.com>
2024-10-14 16:45:13 -0700

    Added missing test cases for an array with only one element and an empty array

--- tail.js
+++ tail.js
@@ -17,6 +17,8 @@ const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lightho
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure the first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
