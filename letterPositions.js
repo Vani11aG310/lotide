44560c18b05c3a7e35f4292baa15157914df2d86
Angelo <angvil02@gmail.com>
2024-10-16 15:02:14 -0700

    Added functionality to be able to skip over spaces

--- letterPositions.js
+++ letterPositions.js
@@ -22,10 +22,12 @@ const assertArraysEqual = function(array1, array2) {
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    if (!results[sentence[i]]) {
-      results[sentence[i]] = [i];
-    } else {
-      results[sentence[i]].push(i);
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
     }
   }
   return (results);
@@ -33,7 +35,6 @@ const letterPositions = function(sentence) {
 
 
 // Test code
-console.log(letterPositions('hello'));
 assertArraysEqual(letterPositions('hello').h, [0]);
 assertArraysEqual(letterPositions('hello').e, [1]);
 assertArraysEqual(letterPositions('hello').l, [2, 3]);
