1c8248827009112904c809c7243c7d79942acf9c
Angelo <angvil02@gmail.com>
2024-10-16 15:13:18 -0700

    Added functionality to findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(arg1, arg2) {
+  if (arg1 === arg2) {
+    console.log(`✅✅✅ Assertion Passed: ${arg1} === ${arg2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arg1} !== ${arg2}`);
+  }
+};
+
+const findKeyByValue = function(objectToSearch, valueToSearchWith) {
+  let founditem;
+  // loop through objectToSearch
+  for (const key in objectToSearch) {
+    if (objectToSearch[key] === valueToSearchWith) {
+      founditem = key;
+    }
+  }
+  return (founditem);
+};
+
+
+// TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Good Doctor"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
