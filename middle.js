d43c73ef4e66ecf518e9c7276d9c5c66bbf87154
Angelo <angvil02@gmail.com>
2024-10-16 12:57:51 -0700

    adjusted names of varaibles to allow for easier readability and understanding of the function

--- middle.js
+++ middle.js
@@ -18,22 +18,22 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-const middle = function(arr) {
+const middle = function(inputArray) {
   let middleArray = [];
   // check if array length is not less than or equal to 2
-  if (arr.length > 2) {
+  if (inputArray.length > 2) {
     // check if array length is not odd
-    if (arr.length % 2 === 0) {
+    if (inputArray.length % 2 === 0) {
       // find the 2 middle items of the array and push them to middleArray
-      const index2 = arr.length / 2;
+      const index2 = inputArray.length / 2;
       const index1 = index2 - 1;
-      middleArray.push(arr[index1], arr[index2])
+      middleArray.push(inputArray[index1], inputArray[index2])
     }
     // check if array length is odd
-    if (arr.length % 2 !== 0) {
+    if (inputArray.length % 2 !== 0) {
       // find the middle term and push to middleArray
-      const index = arr.length / 2 - 0.5;
-      middleArray.push(arr[index])
+      const index = Math.floor(inputArray.length / 2);
+      middleArray.push(inputArray[index])
     }
   }
   return (middleArray);
