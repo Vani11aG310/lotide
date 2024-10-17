72615fb3a56d0a27300e678d2f1bf2bf2bccfa78
Angelo <angvil02@gmail.com>
2024-10-17 14:37:24 -0700

    added test cases to map.js

--- map.js
+++ map.js
@@ -7,5 +7,30 @@ const map = (array, callback) => {
   return results;
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return (false);
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return (false);
+    }
+  }
+  return (true);
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (!eqArrays(array1, array2)) {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  }
+};
+
 const results1 = map(words, (word) => word[0]);
 console.log(results1);
+
+/// Test code
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
+assertArraysEqual(map(words, word => word[0] + word[1]), ['gr', 'co', 'to', 'ma', 'to'])
