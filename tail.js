7055e6ad8052e55e161dcb0c2a93753ba5db9fee
Angelo <angvil02@gmail.com>
2024-10-21 13:05:55 -0700

    refactored tail to have tests in a separate file and to export the tail function

--- tail.js
+++ tail.js
@@ -1,25 +1,7 @@
-const assertEqual = function(arg1, arg2) {
-  if (arg1 === arg2) {
-    console.log(`✅✅✅ Assertion Passed: ${arg1} === ${arg2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arg1} !== ${arg2}`);
-  }
-};
-
 const tail = function(arr) {
   const newArray = arr.slice(1);
   return (newArray);
 };
 
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure the first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
