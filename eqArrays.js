346037ca1a34c88a150f01f1de4e0a203993a5a2
Angelo <angvil02@gmail.com>
2024-10-21 13:11:49 -0700

    refactored eqArrays to export eqArrays and have its tests in its own file

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,14 @@
-const assertEqual = function(arg1, arg2) {
-  if (arg1 === arg2) {
-    console.log(`✅✅✅ Assertion Passed: ${arg1} === ${arg2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arg1} !== ${arg2}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return (false);
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      return (false);
+      return false;
     }
   }
-  return (true);
+  return true;
 };
 
 
-// TEST CODE
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+module.exports = eqArrays;
