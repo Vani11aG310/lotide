d4eab6034a685ab6fe48710e63512ecdb474446a
Angelo <angvil02@gmail.com>
2024-10-21 13:23:00 -0700

    refactored middle to be modular and run its tests in a separate file

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return (false);
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return (false);
-    }
-  }
-  return (true);
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  }
-};
-
 const middle = function(inputArray) {
   let middleArray = [];
   // check if array length is not less than or equal to 2
@@ -35,15 +15,8 @@ const middle = function(inputArray) {
       middleArray.push(inputArray[index])
     }
   }
-  return (middleArray);
+  return middleArray;
 }
 
 
-// TEST CODE
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // => [4, 5]
+module.exports = middle;
